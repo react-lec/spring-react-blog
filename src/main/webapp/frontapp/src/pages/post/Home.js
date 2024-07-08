ae88a714052c4dc65302532b64210c0c8f2ee1f8
getinthere <getinthere@naver.com>
2024-07-08 11:37:19 +0900

    CORS 설정완료 - 게시글 목록 뿌리기 완료

--- src/main/webapp/frontapp/src/pages/post/Home.js
+++ src/main/webapp/frontapp/src/pages/post/Home.js
@@ -1,15 +1,35 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Pagination } from "react-bootstrap";
-import PostItem from "../../components/PostItem";
+import PostItem from "./../../components/PostItem";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    apiHome();
+  }, []);
+
+  async function apiHome() {
+    let response = await axios({
+      url: "http://localhost:8080",
+      method: "get",
+    });
+
+    console.log("posts", response.data);
+    setPosts(response.data.body);
+  }
+
   function prev() {}
   function next() {}
 
   return (
     <div>
-      {<PostItem id={1} title={"제목1"} />}
-      {<PostItem id={2} title={"제목2"} />}
+      {posts.map((post) => (
+        <PostItem key={post.id} id={post.id} title={post.title} />
+      ))}
+
+      {/* {<PostItem id={1} title={"제목1"} />} */}
       <br />
       <div className="d-flex justify-content-center">
         <Pagination>
